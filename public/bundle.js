f4dea05fafc938bcfee796374703b03f743310ed
Adina Matei <adinamateim@gmail.com>
2017-10-26 20:29:09 +0300

    file reorganization

---
+++ public/bundle.js
@@ -0,0 +1,215 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/public/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _square = __webpack_require__(1);
+
+var _square2 = _interopRequireDefault(_square);
+
+var _style = __webpack_require__(2);
+
+var _style2 = _interopRequireDefault(_style);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+//jshint esversion:6
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.getElementById("message");
+var headingColor = document.querySelector('h1');
+var resetButton = document.querySelector('#resetBtn');
+// var easy = document.querySelector("#easyBtn");
+// var hard = document.querySelector("#hardBtn");
+var modeButton = document.querySelectorAll(".mode");
+
+init();
+
+function init() {
+  setupModeButtons();
+  setupSquares();
+  reset();
+}
+
+//setup and add event listener on easy and hard button
+function setupModeButtons() {
+  for (var i = 0; i < modeButton.length; i++) {
+    //add event listener to buttons and remove and add class
+    modeButton[i].addEventListener("click", function () {
+      modeButton[0].classList.remove('selected');
+      modeButton[1].classList.remove('selected');
+      this.classList.add('selected');
+
+      //when click on .mode buttons
+      this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
+      reset();
+    });
+  }
+}
+
+function setupSquares() {
+  for (var i = 0; i < squares.length; i++) {
+    //add event listener to squares
+    squares[i].addEventListener("click", function () {
+      //take color of clicked square
+      var clickedSquare = this.style.backgroundColor;
+
+      //compare clickedSquare with pickedColor
+      if (clickedSquare === pickedColor) {
+        messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?";
+        correctColor(clickedSquare);
+        headingColor.style.backgroundColor = clickedSquare;
+      } else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again!";
+      }
+    });
+  }
+}
+
+function reset() {
+  //generate all new colors
+  colors = generateRandomColors(numSquares);
+  //pick a new random color from array
+  pickedColor = pickRandomColor();
+  //change colorDisplay with pickedColor
+  colorDisplay.textContent = pickedColor;
+  //reset messageDisplay to empty
+  messageDisplay.textContent = "";
+  //reset button
+  resetButton.textContent = "New Colors";
+
+  //change colors of squares
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.display = "block";
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+  headingColor.style.backgroundColor = "#4278ab";
+}
+
+// add event listener on reset button
+resetButton.addEventListener("click", function () {
+  reset();
+});
+
+//when find correct color change the color of all squares
+function correctColor(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+// pick a random color from colors
+function pickRandomColor() {
+  var pickColor = Math.floor(Math.random() * colors.length);
+  return colors[pickColor];
+}
+
+//get random color for number times and push into array
+function generateRandomColors(number) {
+  var color = [];
+  for (var i = 0; i < number; i++) {
+    color.push(randomColor());
+  }
+  return color;
+}
+
+//generate a rgb color
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
